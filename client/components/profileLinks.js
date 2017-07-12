5f29d3ff51ff7befffc3dc51e81ce86b7dc955a8
ibramos <i.am.ian.ramos@gmail.com>
2017-07-12 10:26:10 -0700

    created profileLinks. Needs to be implemented.

---
+++ client/components/profileLinks.js
@@ -0,0 +1,20 @@
+const links = [
+    'https://media.giphy.com/media/tTutsU63rnHC8/giphy.gif', 
+    'https://media.giphy.com/media/Jlys8jzFoI8ne/giphy.gif',
+    'https://media.giphy.com/media/5xtDarpNKCozWZZO61a/giphy.gif',
+    'https://media.giphy.com/media/KGI4gmWK9yPsI/giphy.gif',
+    'https://media.giphy.com/media/FaTrieSsFHcaI/giphy.gif',
+    'https://media.giphy.com/media/t4iLeNiLtG5hK/giphy.gif',
+    'https://media.giphy.com/media/wPFs60amEv3YA/giphy.gif',
+    'https://media.giphy.com/media/Z32XWltSBUpW0/giphy.gif',
+    'https://media.giphy.com/media/10TjYv7tytTjtm/giphy.gif',
+    'https://media.giphy.com/media/IUu7swWWXfeyk/giphy.gif',
+    'https://media.giphy.com/media/hPVKyisRsZAKk/giphy.gif',
+    'https://media.giphy.com/media/129Yiur12UfxNm/giphy.gif',
+    'https://media.giphy.com/media/4P2f56rmCL4VW/giphy.gif',
+    'https://media.giphy.com/media/JYvhRPVLYiE1i/giphy.gif',
+    'https://media.giphy.com/media/12czu2KXaMRiRa/giphy.gif',
+    'https://media.giphy.com/media/yoJC2wqc7suVTag7i8/giphy.gif'
+]
+
+export default links;
